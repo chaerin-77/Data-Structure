114bef6805d052a0d1b362263c244eb214c38ddc
chaerin-77 <74577662+chaerin-77@users.noreply.github.com>
2021-04-13 14:29:46 +0900

    Update singly-linked-list.c

--- singly-linked-list.c
+++ singly-linked-list.c
@@ -13,37 +13,37 @@
 /* 필요한 헤더파일 추가 */
 
 typedef struct Node {
-	int key;
-	struct Node* link;
+	int key; // node의 data
+	struct Node* link; // 다음 node를 가리키는 포인터
 } listNode;
 
 typedef struct Head {
-	struct Node* first;
+	struct Node* first; // head 포인터인 first
 }headNode;
 
 
 /* 함수 리스트 */
-headNode* initialize(headNode* h);
-int freeList(headNode* h);
+headNode* initialize(headNode* h); // first 포인터를 사용하여 리스트 초기화
+int freeList(headNode* h); // 사용이 끝난 리스트 해제
 
-int insertFirst(headNode* h, int key);
-int insertNode(headNode* h, int key);
-int insertLast(headNode* h, int key);
+int insertFirst(headNode* h, int key); // 데이터의 크기와 상관없이 리스트의 맨 앞에 노드 추가
+int insertNode(headNode* h, int key); // 데이터의 크기를 비교하여 오름차순으로 노드 추가
+int insertLast(headNode* h, int key); // 데이터의 크기와 상관없이 리스트의 맨 뒤에 노드 추가
 
-int deleteFirst(headNode* h);
-int deleteNode(headNode* h, int key);
-int deleteLast(headNode* h);
-int invertList(headNode* h);
+int deleteFirst(headNode* h); // 맨 앞 노드 삭제
+int deleteNode(headNode* h, int key); // 검색한 데이터와 같은 값을 가지고 있는 노드 삭제
+int deleteLast(headNode* h); // 맨 뒤 노드 삭제
+int invertList(headNode* h); // 리스트를 역순으로 정렬
 
-void printList(headNode* h);
+void printList(headNode* h); // 리스트를 출력
 
 int main()
 {
 	printf("[----- [Chaerin Jung] [2020039022] -----]\n\n");
 	
-	char command;
-	int key;
-	headNode* headnode=NULL;
+	char command; // 선택한 메뉴를 받을 변수 선언
+	int key; // 입력할 데이터를 받을 변수 선언
+	headNode* headnode=NULL; // 맨 앞 노드를 null로 초기화
 
 	do{
 		printf("----------------------------------------------------------------\n");
@@ -61,42 +61,42 @@ int main()
 
 		switch(command) {
 		case 'z': case 'Z':
-			headnode = initialize(headnode);
+			headnode = initialize(headnode); // headnode를 사용하여 리스트 초기화
 			break;
 		case 'p': case 'P':
-			printList(headnode);
+			printList(headnode); // 리스트 출력
 			break;
 		case 'i': case 'I':
 			printf("Your Key = ");
-			scanf("%d", &key);
-			insertNode(headnode, key);
+			scanf("%d", &key); // 리스트에 추가할 데이터값을 입력받음
+			insertNode(headnode, key); // 리스트에 오름차순으로 추가
 			break;
 		case 'd': case 'D':
 			printf("Your Key = ");
-			scanf("%d", &key);
-			deleteNode(headnode, key);
+			scanf("%d", &key); // 해당되는 데이터를 가진 노드를 삭제하기 위한 값을 입력받음
+			deleteNode(headnode, key); // 리스트에 검색하여 해당되는 노드 삭제
 			break;
 		case 'n': case 'N':
 			printf("Your Key = ");
-			scanf("%d", &key);
-			insertLast(headnode, key);
+			scanf("%d", &key); // 리스트에 추가할 데이터값을 입력받음
+			insertLast(headnode, key); // 리스트의 마지막에 노드 추가
 			break;
 		case 'e': case 'E':
-			deleteLast(headnode);
+			deleteLast(headnode); // 리스트의 마지막 노드 삭제
 			break;
 		case 'f': case 'F':
 			printf("Your Key = ");
-			scanf("%d", &key);
-			insertFirst(headnode, key);
+			scanf("%d", &key); // 리스트에 추가할 데이터값을 입력받음
+			insertFirst(headnode, key); // 리스트의 처음에 노드 추가
 			break;
 		case 't': case 'T':
-			deleteFirst(headnode);
+			deleteFirst(headnode); // 리스트의 처음 노드 삭제
 			break;
 		case 'r': case 'R':
-			invertList(headnode);
+			invertList(headnode); // 리스트를 역순으로 정렬
 			break;
 		case 'q': case 'Q':
-			freeList(headnode);
+			freeList(headnode); // 종료 시 사용한 리스트 해제
 			break;
 		default:
 			printf("\n       >>>>>   Concentration!!   <<<<<     \n");
@@ -130,9 +130,9 @@ int freeList(headNode* h){
 	while(p != NULL) {
 		prev = p;
 		p = p->link;
-		free(prev);
+		free(prev); // h와 연결된 리스트 해제
 	}
-	free(h);
+	free(h); // 마지막으로 h도 해제
 	return 0;
 }
 
@@ -143,11 +143,11 @@ int freeList(headNode* h){
  */
 int insertFirst(headNode* h, int key) {
 
-	listNode* node = (listNode*)malloc(sizeof(listNode));
-	node->key = key;
+	listNode* node = (listNode*)malloc(sizeof(listNode)); // 입력된 데이터값을 가지는 노드를 만들기 위해 동적할당으로 노드 생성
+	node->key = key; // 생성된 노드의 데이터 값에 입력받은 데이터 입력
 
-	node->link = h->first;
-	h->first = node;
+	node->link = h->first; // 새로 추가된 노드의 link에 first가 가리키고 있는 노드의 주소값 대입
+	h->first = node; // first가 가리키고 있는 노드를 새로 추가된 노드로 변경
 
 	return 0;
 }
@@ -158,36 +158,37 @@ int insertNode(headNode* h, int key) {
 
 	listNode* n;
 	listNode* trail = NULL;
-	listNode* node = (listNode*)malloc(sizeof(listNode));
-	n = h->first;
-	node->key = key;
+	listNode* node = (listNode*)malloc(sizeof(listNode)); // 새로 추가할 노드
+	n = h->first; // 처음 n의 값은 first가 가리키고 있는 노드
+	node->key = key; // 새로 생성한 노드의 데이터 값에 입력받은 데이터값 대입
 
 	if(n == NULL){
-		h->first = node;
-		node->link = NULL;
+		h->first = node; // 만약 리스트가 비어있었다면 first가 가리키고 있는 노드를 node로 변경
+		node->link = NULL; // node의 link는 NULL값으로 변경
 	}
 	
 	else{
 		
-		if(n->key >= node->key){
-			node->link = n;
-			h->first = node;
+		if(h->first->key >= node->key){// 만약 입력받은 키 값이 n이 가리키고 있는 키 값보다 작으면 바로 추가
+			node->link = n; // 새로 추가할 node의 link를 n을 가리키도록 수정
+			h->first = node; // first가 가리키는 노드를 node로 변경
 		}
 
 		else{
 			
-			while(n->key < node->key){
-				if (n->link == NULL && n->key <= node->key){
-					node->link = n->link;
-					n->link = node;
+			while(n->key < node->key){ // 입력받은 키 값이 n이 가리키고 있는 키 값보다 클 때까지
+				if (n->link == NULL && n->key <= node->key){ // 즉 새로 추가할 node의 키 값이 현재 리스트의 모든 키 값보다 클 경우
+					node->link = n->link; // node의 link를 n의 링크 즉 NULL로 변경
+					n->link = node; // n의 link를 node를 가리키도록 변경
 					return 0;
 				}
-				trail = n;
-				n = n->link;
+				trail = n; // 비교할 노드 바로 전의 노드를 가리킬 포인터 trail의 값을 n으로 하나 증가
+				n = n->link; // n은 뒤의 노드를 가리키도록 변경
 			}
-		
-			trail->link = node;
-			node->link = n;
+			// 리스트를 하나씩 비교하다 입력 받은 키 값보다 n이 가리키는 키 값이 더 클 경우
+
+			trail->link = node; // 이전 노드의 링크가 node를 가리키도록 변경
+			node->link = n;// node의 link가 n을 가리키도록 변경
 		}
 	}
 
@@ -203,19 +204,19 @@ int insertLast(headNode* h, int key) {
 	listNode* n;
 	n = h->first;
 	node->key = key;
-	node->link = NULL;
+	node->link = NULL; // 마지막에 추가할 노드이기 때문에 node의 링크는 항상 NULL 이다.
 
 	if(n == NULL)
 	{
-		h->first = node;
+		h->first = node; // 리스트가 비어있다면 바로 추가
 	}
 
 	else
 	{
-		while(n->link != NULL)
-			n = n->link;
+		while(n->link != NULL) // 즉, 현재 비교하고 있는 노드가 리스트의 끝이 아닐때까지 
+			n = n->link; // n을 다음 노드로 변경
 		
-		n->link = node;
+		n->link = node; // 현재 비교하는 노드가 끝이되었으면 뒤에 node 추가
 	}
 
 	return 0;
@@ -231,11 +232,11 @@ int deleteFirst(headNode* h) {
 	n = h->first;
 
 	if(n == NULL)
-		printf("Linked list is Empty!!");
+		printf("Linked list is Empty!!\n\n"); // 리스트가 비어있으면 비어있다는 오류 문자열 출력
 	
 	else{
-		h->first = n->link;
-		free(n);
+		h->first = n->link; // 첫 번째 노드를 삭제하기 때문에 first가 가리키는 값을 n의 다음 노드 값으로 변경한 후
+		free(n); // n의 메모리 해제
 	}
 
 	return 0;
@@ -252,23 +253,24 @@ int deleteNode(headNode* h, int key) {
 	n = h->first;
 
 	if(n == NULL)
-		printf("Linked list is Empty!!");
+		printf("Linked list is Empty!!\n\n"); // 리스트가 비어있으면 비어있다는 오류 문자열 출력
 	
 	else{
-		if(n->key == key){
-			h->first = n->link;
-			free(n);
+		if(n->key == key){ // 만약 첫번째 노드의 키 값이 삭제할 노드의 키 값과 같다면
+			h->first = n->link; // first가 가리키는 노드를 n의 다음 노드로 변경 후
+			free(n); // n 해제
 		}
 
 		else
 		{
-			while(n->key != key){
-				trail = n;
-				n = n->link;
+			while(n->key != key){ // n의 키 값이 삭제할 키값과 같지 않을 때까지
+				trail = n; // 이전 노드를 가리키는 trail 값을 n으로 변경
+				n = n->link; // n은 n의 다음 노드를 가리키도록 변경
 			}
-		
-			trail->link = n->link;
-			free(n);
+			// n의 키 값이 삭제할 키 값과 같다면
+
+			trail->link = n->link; // 이전 노드의 link 값을 n의 다음 노드를 가리키도록 변경
+			free(n); //n 해제
 		}
 	}
 
@@ -285,21 +287,24 @@ int deleteLast(headNode* h) {
 	trail = NULL;
 
 	if(n == NULL)
-		printf("Linked list is Empty!!");
+		printf("Linked list is Empty!!\n\n"); // 리스트가 비어있으면 비어있다는 오류 문자열 출력
 	
 	else{
-		if(n->link == NULL){
-			
+		if(h->first->link == NULL){ // 리스트에 노드가 하나 남아있다면
+			h->first = NULL; // first가 가리키고 있는 값 NULL로 변경
+			free(n); // n 해제
+			return 0;
 		}
-		while(n->link != NULL){
-			trail = n;
-			n = n->link;
-		}
-		
-		trail->link = NULL;
-		free(n);
-	}
 
+		while(n->link != NULL){ // 리스트의 마지막 노드까지
+			trail = n; // 이전 노드를 가리키는 trail의 값을 n으로 변경
+			n = n->link; // n이 다음 노드를 가리키도록 변경
+		}
+		// 리스트의 마지막 노드라면
+
+		trail->link = NULL; // 이전 노드의 link를 NULL로 변경
+		free(n); // 마지막 노드인 n 해제
+	}
 
 	return 0;
 }
@@ -310,17 +315,17 @@ int deleteLast(headNode* h) {
  */
 int invertList(headNode* h) {
 
-	listNode* next, *trail, *n;
+	listNode* next, *trail, *n; // 다음 노드, 이전 노드, 현재 노드를 가리킬 포인터 선언
 	next = trail = NULL;
 	n = h->first;
 
 	while(n){
-		next = n->link;
-		n->link = trail;
-		trail = n;
-		n = next;
+		next = n->link; // next가 현재 노드의 다음 노드를 가리키도록 변경
+		n->link = trail; // 현재 노드의 link는 이전 노드를 가리키도록 변경
+		trail = n; // 이전 노드는 현재 노드를 가리키도록 변경
+		n = next; // 현재 노드 n은 다음 노드를 가리키도록 변경
 	}
-	h->first = trail;
+	h->first = trail; // 마지막으로 first가 가리키는 값을 trail로 변경
 
 	return 0;
 }
@@ -332,19 +337,18 @@ void printList(headNode* h) {
 
 	printf("\n---PRINT\n");
 
-	if(h == NULL) {
-		printf("Nothing to print....\n");
+	if(h == NULL) {// 헤드 포인터가 NULL이라면
+		printf("Nothing to print....\n"); 
 		return;
 	}
 
 	p = h->first;
 
-	while(p != NULL) {
-		printf("[ [%d]=%d ] ", i, p->key);
-		p = p->link;
-		i++;
+	while(p != NULL) { // p가 가리키는 값이 NULL이 아닐 때 까지
+		printf("[ [%d]=%d ] ", i, p->key); // 노드의 데이터 출력
+		p = p->link; // p가 p의 다음 노드를 가리키도록 변경
+		i++; // 데이터의 번호 하나 증가
 	}
 
-	printf("  items = %d\n", i);
+	printf("  items = %d\n", i); // 총 데이터의 갯수 출력
 }
-
